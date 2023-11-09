f25df9e8e9ddf12666eab5da256fccd4993dd05a
ZaneMJohnson <johnsonzanem@gmail.com>
2023-11-08 18:03:49 -0800

    adding-player

--- game.py
+++ game.py
@@ -99,6 +99,6 @@ def play(game, x_player, o_player, print_game=True):
 if __name__ == '__main__':
     x_player = HumanPlayer('X')
     o_player = HumanPlayer2('O')
-    # o_player = RandomComputerPlayer('O')
+    o_player = RandomComputerPlayer('O')
     t = TicTacToe() 
     play(t, x_player, o_player, print_game=True)
--- player.py
+++ player.py
@@ -36,5 +36,23 @@ class HumanPlayer(Player):
                 valid_square = True
             except ValueError:
                 print('Invalid square. Try again.')
+
+class HumanPlayer2(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+
+    def get_move(self, game):
+        valid_square = False
+        val = None
+        while not valid_square:
+            square = input(self.letter + '\'s turn input (0-9)')
+
+            try:
+                val = int(square)
+                if val not in game.available_moves():
+                    raise ValueError
+                valid_square = True
+            except ValueError:
+                print('Invalid square. Try again.')
             
         return val
